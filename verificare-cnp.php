5b0dafbc3c250cc68d4870fbfffad5f8f25c1a37
Merla Lucian <35219288+merlalucian@users.noreply.github.com>
2021-08-16 14:07:53 +0300

    Add files via upload

---
+++ verificare-cnp.php
@@ -0,0 +1,83 @@
+<?php
+function verificareCNP($cnp)
+{
+
+    if (preg_match('#[^0-9]#', $cnp)) {
+        return false;
+    }
+
+    $cnp = str_split($cnp);
+
+    if (count($cnp) != 13) {
+        return false;
+    }
+
+    if ($cnp[0] < 1 || $cnp[0] > 9) {
+        return false;
+    }
+
+    $luna_nasterii = $cnp[3] . $cnp[4];
+
+    if ($cnp[4] == 0 || $cnp[3] > 1) {
+        return false;
+    }
+
+    if ($luna_nasterii > 12) {
+        return false;
+    }
+
+    if ($cnp[5] > 3 || $cnp[6] == 0) {
+        return false;
+    }
+
+    $ziua_nasterii = $cnp[5] . $cnp[6];
+
+    if ($ziua_nasterii > 31) {
+
+        return false;
+    }
+
+    $judet = $cnp[7] . $cnp[8];
+    if ($judet > 52) {
+
+        return false;
+    }
+
+    if ($cnp[8] == 0) {
+
+        return false;
+    }
+
+    $numar_control = 279146358279;
+    $suma_totala = 0;
+    $numar_control = str_split($numar_control);
+
+    if ($cnp[11] == 0) {
+        return false;
+    }
+
+    $cnp_temp = $cnp;
+    unset($cnp_temp[12]);
+
+    foreach ($numar_control as $key => $value) {
+
+        $suma_totala += intval($value) * intval($cnp_temp[$key]);
+    }
+
+    $control = $suma_totala % 11;
+
+    if ($control != $cnp[12]) {
+
+        return false;
+    }
+
+    return true;
+}
+
+$cnp = '';
+$check =  verificareCNP($cnp);
+if (empty($check)) {
+    echo "CNP Invalid";
+} else {
+    echo "CNP Valid";
+}
